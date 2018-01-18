87e387d804c531b4266b6df9eba2269f72f3740b
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2018-01-18 10:11:42 +0000

    Add basic entities

---
+++ LeedsSharp.ElasticSearch.Common/Models/Entities/SocialChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeedsSharp.ElasticSearch.Common.Models.Entities
+{
+	public class SocialChannel
+	{
+		public Guid UserId { get; set; }
+		public string Name { get; set; }
+		public string Url { get; set; }
+		public string Followers { get; set; }
+	}
+}
