656fc4fc3592202b914f51d84a29c7c78d7dfaef
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2018-01-24 16:20:12 +0000

    Rename service reference for clarity

--- LeedsSharp.ElasticSearch.Common/Services/IndexingService.cs
+++ LeedsSharp.ElasticSearch.Common/Services/IndexingService.cs
@@ -11,25 +11,25 @@ namespace LeedsSharp.ElasticSearch.Common.Services
 {
 	public class IndexingService
 	{
-        public static IElasticClient EsClient => SearchClientHelper.GetClient();
+        public static IElasticClient ElasticClient => SearchClientHelper.GetClient();
 		private static readonly ILog Log = LogManager.GetLogger(typeof(IndexingService));
 
 		public async Task AddItemToIndexAsync(User item)
 		{
-			await EsClient.IndexAsync(item, d => d.Index("user"));
+			await ElasticClient.IndexAsync(item, d => d.Index("user"));
 		}
 
 		public async Task AddItemsToIndexAsync(IEnumerable<User> items)
 		{
 			if (items.Count() == 1)
 			{
-				await EsClient.IndexAsync(items.First(), d => d.Index("user"));
+				await ElasticClient.IndexAsync(items.First(), d => d.Index("user"));
 			}
 			else
 			{
 				Parallel.ForEach(items, async (x, y) =>
 				{
-					await EsClient.IndexAsync(x, d => d.Index("user"));
+					await ElasticClient.IndexAsync(x, d => d.Index("user"));
 				});
 			}
 		}
@@ -38,7 +38,7 @@ namespace LeedsSharp.ElasticSearch.Common.Services
 		{
 			foreach (var item in items)
 			{
-				var response = await EsClient.UpdateAsync(DocumentPath<User>.Id(item.Id), x => x.Index("user").Doc(item));
+				var response = await ElasticClient.UpdateAsync(DocumentPath<User>.Id(item.Id), x => x.Index("user").Doc(item));
 				if (!response.IsValid)
 				{
 					switch (response.ServerError?.Status)
@@ -61,7 +61,7 @@ namespace LeedsSharp.ElasticSearch.Common.Services
 		{
 			try
 			{
-				var response = await EsClient.IndexExistsAsync("user");
+				var response = await ElasticClient.IndexExistsAsync("user");
 
 				return response.Exists;
 			}
@@ -75,7 +75,7 @@ namespace LeedsSharp.ElasticSearch.Common.Services
 		{
 			try
 			{
-				var response = await EsClient.DeleteIndexAsync("user");
+				var response = await ElasticClient.DeleteIndexAsync("user");
 			}
 			catch (Exception ex)
 			{
@@ -87,7 +87,7 @@ namespace LeedsSharp.ElasticSearch.Common.Services
 		{
 			try
 			{
-				var response = await EsClient
+				var response = await ElasticClient
 					.CreateIndexAsync("user", d => d
 						.InitializeUsing(new CreateIndexRequest("user") { Settings = GetDefaultSettings() })
 						.Mappings(m => m.Map<User>(x => x.AutoMap())
